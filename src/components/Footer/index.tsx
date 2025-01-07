1595d9b4a1f1d0eb9c99fd6231f557149671d17b
Emanuel Bitencourt <emanuelmagalhaes3m@gmail.com>
2025-01-07 15:25:13 -0300

    reformulando footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,20 @@
+import Link from "next/link";
+
 export function Footer() {
   return (
-    <footer className=" h-fit w-full    items-end px-14 ">
-      <div className="flex justify-between py-4">
+    <footer className=" h-fit w-full  px-14 ">
+      <div className="flex flex-col py-4 justify-center text-center">
         <div>
-          <h1 className="fontTsuki text-2xl">AnimaSancta</h1>
+          <h1 className="fontTsuki text-xl">AnimaSancta</h1>
         </div>
-        <div>Contatos</div>
+        <div>
+          <Link
+            href={"https://www.linkedin.com/in/emanuelbitencourt/"}
+            className="underline "
+          >
+            Direitos Reservados: Bitenc
+          </Link>
+        </div>
       </div>
     </footer>
   );
