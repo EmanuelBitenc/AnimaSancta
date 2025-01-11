dda5895f26fb40a648873a5198ff6d1e716100c9
Emanuel Bitencourt <emanuelmagalhaes3m@gmail.com>
2025-01-11 19:59:12 -0300

    pagina notFound

---
+++ src/app/not-found.tsx
@@ -0,0 +1,15 @@
+import "@/app/globals.css";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="min-h-[calc(100vh-500px)] text-center py-10 ">
+      <h1 className="mx-auto my-auto mb-4 text-amber-950">
+        404 - Página não encontrada
+      </h1>
+      <div className=" bg-amber-950 mx-auto w-11/12 sm:w-3/4 md:w-2/4 lg:w-1/4 rounded-lg p-3">
+        <Link href="/">Voltar para Home</Link>
+      </div>
+    </div>
+  );
+}
