30e789a2750098a6f516bd39873a40dd885f0fa4
Emanuel Bitencourt <emanuelmagalhaes3m@gmail.com>
2025-01-11 21:12:47 -0300

    adicionando exameConciencia como home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import "./globals.css";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  redirect("/exameConciencia");
   return <div className="min-h-[calc(100vh-500px)]"></div>;
 }
--- src/components/Header/nav.tsx
+++ src/components/Header/nav.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 export function Nav() {
   return (
     <div className=" py-2 flex gap-2 sm:gap-10 flex-col sm:flex-row justify-center sm:justify-between  flex-wrap px-2 sm:px-14 self-center items-center  ">
-      <Link href="/">
+      <Link href="/exameConciencia">
         <h1 className="fontTsuki text-2xl ">AnimaSancta</h1>
       </Link>
       <nav>
         <ul className="flex sm:gap-4   ">
           <li>
             <Link
-              href="/"
+              href="/exameConciencia"
               className="px-2 sm:px-4 py-2   hover:underline hover:drop-shadow"
             >
               Home
