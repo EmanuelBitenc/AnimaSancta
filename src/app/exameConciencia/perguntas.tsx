bf5a769a7fcd5c58006ec2e78b0528ec61f54318
Emanuel Bitencourt <emanuelmagalhaes3m@gmail.com>
2025-03-24 16:41:27 -0300

    correção nomeclatura

--- src/app/exameConciencia/perguntas.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import { Pergunta } from "@/data/ListaExameConsciencia";
-import { UseFormReturn, FieldValues } from "react-hook-form";
-import { useState } from "react";
-
-export const Perguntas = () => {
-  return <div></div>;
-};
